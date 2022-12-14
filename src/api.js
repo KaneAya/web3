9d90e8d556c88cf66eec051a5e091847ea49d4ce
KaneAya <115447877+KaneAya@users.noreply.github.com>
2022-12-14 11:24:32 +0900

    Delete api.js

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(breed) {
-  const response = await fetch(
-      `https://randomfox.ca\/?i=${breed}.jpg`
-  );
-  const data = await response.json();
-  return data.message;
-}
